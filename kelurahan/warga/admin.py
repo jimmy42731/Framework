0315a8ce8acf067ecedc251c77021135c0f9111f
jimmy <156490154+jimmy42731@users.noreply.github.com>
2025-11-12 20:35:30 +0700

    Delete kelurahan directory

--- kelurahan/warga/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Warga
-from .models import Pengaduan
-
-# Register your models here.
-admin.site.register(Warga)
-admin.site.register(Pengaduan)
